3830ce1b99e7eb802621e213bb5c80c341778384
pouya <pouya.May4@gmail.com>
2024-05-09 01:09:21 +0330

    imake search box but i have problem

--- js/app.js
+++ js/app.js
@@ -1,60 +1,50 @@
+let bookList = [];
 let table = document.querySelector("#table");
 const form = document.querySelector("#form");
-let firstTr = document.querySelector("#firstTr");
-let bookList = [];
-form.addEventListener("submit", (e) => {
-  e.preventDefault()
-  let newBook = {
+const searchBox = document.querySelector('#search')
+
+
+form.addEventListener("submit", (e)=>
+{
+  
+  let newBook = 
+  {
     bookName: e.target.bookName.value,
     bookPublisher: e.target.bookPublisher.value,
     author: e.target.author.value,
     bookPrice: e.target.bookPrice.value,
     janre: e.target.janre.value,
   };
-  for(let i = 0; i < 3;i++)
-  {
-    if(i == 2)
-    {
-      bookList.shift()
-      
-    }
-    console.log(i)
-  }
   bookList.push(newBook);
   console.log(bookList);
-  createTableRows()
-  
+  renderInUi(bookList,table)
+  e.preventDefault()
 });
 
-function createTableRows()
-{
-  for ( let i = 0; i < bookList.length; i++ )
-  {
-    const row = document.createElement("tr");
-  
-    const bookNameCall = document.createElement("td");
-    bookNameCall.textContent = bookList[i].bookName;
-  
-    const bookPublisherCall = document.createElement("td");
-    bookPublisherCall.textContent = bookList[i].bookPublisher;
-  
-    const authorCall = document.createElement("td");
-    authorCall.textContent = bookList[i].author;
 
-    const janreCall = document.createElement("td");
-    janreCall.textContent = bookList[i].janre;
+function renderInUi(contentArry,container)
+{
+  container.innerHTML = '';
+  contentArry.forEach((item)=>
+  {
+    const row = document.createElement('tr');
+   row.innerHTML = `<td>${item.bookName}</td><td>${item.bookPublisher}</td><td>${item.author}</td><td>${item.bookPrice}</td><td>${item.janre}</td>` 
   
-    const bookPriceCall = document.createElement("td");
-    bookPriceCall.textContent = bookList[i].bookPrice;
+   container.appendChild(row)
+    
+  });
   
-    row.appendChild(bookNameCall);
-    row.appendChild(bookPublisherCall);
-    row.appendChild(authorCall);
-    row.appendChild(janreCall);
-    row.appendChild(bookPriceCall);
-  
-    table.appendChild(row);
-  }
   
 }
+let searchResult = bookList;
+searchBox.addEventListener('input',(e)=>
+{
+  const userSearchText = e.target.value.toLowerCase();
+  searchResult = bookList.fill((item)=>
+  {
+    return item.toLowerCase().includes(userSearchText);
+  })
+  renderInUi(searchResult,table)
+})
 
+renderInUi(searchResult,table)
